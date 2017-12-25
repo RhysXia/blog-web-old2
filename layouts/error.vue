91f2726bbde88737d9ee4e969665f1f7c6a6a988
Ryths Xia <xrs4433@outlook.com>
2017-12-25 15:46:19 +0800

    完成错误页

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,66 +1,52 @@
-<template>
-  <div class="error-page">
-    <div>
-      <h1 class="error-code">{{ error.statusCode }}</h1>
-      <div class="error-wrapper-message">
-        <h2 class="error-message">{{ error.message }}</h2>
-      </div>
-      <p v-if="error.statusCode === 404"><nuxt-link class="error-link" to="/">Back to the home page</nuxt-link></p>
-    </div>
-  </div>
+<template lang="pug">
+    .error-page
+        .error-wrapper
+            h1.error-code {{ error.statusCode }}
+            .error-wrapper-message
+                h2.error-message {{ error.message }}
+            p(v-if="error.statusCode === 404")
+                nuxt-link.error-link(to="/") Back to the home page
 </template>
-
 <script>
-  export default {
-    name: 'nuxt-error',
-    props: ['error'],
-    head () {
-      return {
-        title: this.error.message || 'An error occured'
-      }
-    }
-  }
-</script>
 
-<style scoped>
-  .error-page {
-    color: #000;
-    background: #fff;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    position: absolute;
-    font-family: "SF UI Text", "Helvetica Neue", "Lucida Grande";
-    text-align: center;
-    padding-top: 20%;
-  }
-  .error-code {
-    display: inline-block;
-    font-size: 24px;
-    font-weight: 500;
-    vertical-align: top;
-    border-right: 1px solid rgba(0, 0, 0, 0.298039);
-    margin: 0px 20px 0px 0px;
-    padding: 10px 23px;
-  }
-  .error-wrapper-message {
-    display: inline-block;
-    text-align: left;
-    line-height: 49px;
-    height: 49px;
-    vertical-align: middle;
-  }
-  .error-message {
-    font-size: 14px;
-    font-weight: normal;
-    margin: 0px;
-    padding: 0px;
-  }
-  .error-link {
-    color: #00BCD4;
-    font-weight: normal;
-    text-decoration: none;
-    font-size: 14px;
-  }
+    export default {
+        name: 'nuxt-error',
+        props: ['error'],
+        head() {
+            return {
+                title: this.error.message || 'An error occured'
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    @import "~assets/scss/variables";
+
+    .error-page {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background: $color-background;
+        .error-wrapper {
+            position: absolute;
+            display: inline-block;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -70%);
+            text-align: center;
+            .error-code{
+                font-size: 5rem;
+                border-bottom: 1px solid $color-border-base;
+                margin: 0;
+            }
+            .error-wrapper-message{
+                .error-message{
+                    font-size: 2rem;
+                }
+            }
+        }
+    }
+
 </style>
