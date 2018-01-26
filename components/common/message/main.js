53141a94f5138f8d94a16c83c083514520a4ead4
Ryths Xia <xrs4433@outlook.com>
2018-01-26 13:35:30 +0800

    update

--- components/common/message/main.js
+++ components/common/message/main.js
@@ -6,50 +6,50 @@ let seed = 1
 let instances = []
 let zIndex = 2000
 const Message = function (options) {
-    if (Vue.prototype.$isServer) {
-        return
+  if (Vue.prototype.$isServer) {
+    return
+  }
+  if (typeof options === 'string') {
+    options = {
+      content: options
     }
-    if (typeof options === 'string') {
-        options = {
-            content: options
-        }
-    }
-    let id = 'message_' + seed++
-    let userOnClose = options.onClose
-    options.onClose = function () {
-        Message.close(id, userOnClose)
-    }
+  }
+  let id = 'message_' + seed++
+  let userOnClose = options.onClose
+  options.onClose = function () {
+    Message.close(id, userOnClose)
+  }
 
-    const instance = new MessageConstructor({
-        propsData: options
-    })
-    instance.id = id
-    instance.vm = instance.$mount()
-    document.body.appendChild(instance.vm.$el)
-    instance.vm.visible = true
-    instance.vm.$el.style.zIndex = `${zIndex++}`
-    instances.push(instance)
-    return instance.vm
+  const instance = new MessageConstructor({
+    propsData: options
+  })
+  instance.id = id
+  instance.vm = instance.$mount()
+  document.body.appendChild(instance.vm.$el)
+  instance.vm.visible = true
+  instance.vm.$el.style.zIndex = `${zIndex++}`
+  instances.push(instance)
+  return instance.vm
 }
 
 Message.close = (id, userOnClose) => {
-    for (let i = 0, len = instances.length; i < len; i++) {
-        if (id === instances[i].id) {
-            if (typeof userOnClose === 'function') {
-                userOnClose(instances[i])
-            }
-            instances[i].vm.$destroy()
-            document.body.removeChild(instances[i].vm.$el)
-            instances.splice(i, 1)
-            break
-        }
+  for (let i = 0, len = instances.length; i < len; i++) {
+    if (id === instances[i].id) {
+      if (typeof userOnClose === 'function') {
+        userOnClose(instances[i])
+      }
+      instances[i].vm.$destroy()
+      document.body.removeChild(instances[i].vm.$el)
+      instances.splice(i, 1)
+      break
     }
+  }
 }
 
 Message.closeAll = function () {
-    for (let i = instances.length - 1; i >= 0; i--) {
-        instances[i].close()
-    }
+  for (let i = instances.length - 1; i >= 0; i--) {
+    instances[i].close()
+  }
 }
 
 export default Message
--- pages/user/_id/index.vue
+++ pages/user/_id/index.vue
@@ -2,20 +2,33 @@
     .self-info-container
         template(v-if="drafts.length>0")
             h2.title 草稿
-            c-draft-item.item(v-for="draft in drafts",:key="draft.id",:draft="draft")
+            .wrapper
+                c-draft-item.item(@delete="deleteDraft(draft.id)",v-for="draft in drafts",:key="draft.id",:draft="draft")
         template(v-if="categories.length>0")
             h2.title 常用分类
-            c-category-item.item(v-for="category in categories",:key="category.id",:category="category")
-
+            .wrapper
+                c-category-item.item(@update="categoryUpdate(category)",@delete="deleteCategory(category.id)",v-for="category in categories",:key="category.id",:category="category")
         template(v-if="articles.length>0")
             h2.title 最近文章
-            c-article-item.item(v-for="article in articles",:key="article.id",:article="article")
+            c-article-item.item.article(v-for="article in articles",:key="article.id",:article="article")
+        c-modal(v-model="isModal",title="修改分类")
+            .body
+                .input-wrapper
+                    label.label 名称
+                    c-input(v-model="category.name",placeholder="请输入分类名称")
+                .input-wrapper
+                    label.label 描述
+                    c-input(v-model="category.description",placeholder="请输入分类描述")
+            .footer(slot="footer")
+                button.primary(@click="categorySubmit") 确定
+                button(@click="isModal=false") 取消
 </template>
-
 <script>
   import CArticleItem from '~/components/article/item'
   import CCategoryItem from '~/components/category/item'
   import CDraftItem from '~/components/draft/item'
+  import CModal from '~/components/common/modal'
+  import CInput from '~/components/common/input'
 
   export default {
     name: 'info',
@@ -25,7 +38,8 @@
         const data = {
           categories: [],
           drafts: [],
-          articles: []
+          articles: [],
+          userId
         }
         let res = await store.$api.category.getAllByUserId({
           userId,
@@ -52,23 +66,128 @@
         error({statusCode: err.statusCode, message: err.message})
       }
     },
+    data () {
+      return {
+        isModal: false,
+        category: {
+          id: -1,
+          name: '',
+          description: '',
+          weight: 0
+        }
+      }
+    },
+    methods: {
+      async categorySubmit () {
+        try {
+          await this.$api.category.update(this.category)
+
+          this.$message({
+            content: '修改成功',
+            duration: 2000,
+            type: 'success'
+          })
+        } catch (err) {
+          this.$message({
+            content: err.message,
+            duration: 2000,
+            type: 'error'
+          })
+        }
+      },
+      categoryUpdate (category) {
+        this.category.id = category.id
+        this.category.name = category.name
+        this.category.description = category.description
+        this.category.weight = category.weight
+        this.isModal = true
+      },
+      async deleteDraft (id) {
+        try {
+          await this.$api.draft.deleteById(id)
+          this.$message({
+            content: '删除成功',
+            duration: 2000,
+            type: 'success'
+          })
+          let res = await this.$api.draft.getSelf({page: 0, size: 100})
+          this.drafts = res.data.content
+        } catch (err) {
+          this.$message({
+            content: err.message,
+            duration: 2000,
+            type: 'error'
+          })
+        }
+      },
+      async deleteCategory (id) {
+        try {
+          await this.$api.category.deleteById(id)
+          const userId = this.userId
+          let res = await this.$api.category.getAllByUserId({
+            userId,
+            page: 0,
+            size: 4,
+            sort: 'weight,DESC'
+          })
+          this.categories = res.data.content
+          this.$message({
+            content: '删除成功',
+            duration: 2000,
+            type: 'success'
+          })
+        } catch (err) {
+          this.$message({
+            content: err.message,
+            duration: 2000,
+            type: 'error'
+          })
+        }
+      }
+    },
     components: {
       CArticleItem,
       CCategoryItem,
-      CDraftItem
+      CDraftItem,
+      CModal,
+      CInput
     }
   }
 </script>
 
 <style lang="scss" scoped>
     @import "~assets/scss/variables";
+    @import "~assets/scss/mixins";
 
     .self-info-container {
         .title {
             font-size: 1.1em;
         }
-        .item{
+        .item {
             margin-bottom: 1em;
+        }
+        .wrapper {
+            margin-right: -1em;
+            @include clearfix;
+            .item {
+                box-sizing: border-box;
+                width: calc(50% - 1em);
+                float: left;
+                margin-right: 1em;
+            }
+        }
+        .article {
+            font-size: 0.8em;
+        }
+        .body {
+            .input-wrapper {
+                margin-bottom: 1em;
+            }
+        }
+        .footer {
+            .primary {
+                margin-right: 1em;
+            }
         }
     }
 </style>
