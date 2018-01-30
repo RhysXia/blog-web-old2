a778db48ddb147a8e89f63f1d2389d96279be0ae
Ryths Xia <xrs4433@outlook.com>
2018-01-30 19:05:20 +0800

    完成modal组件的开发

--- components/common/modal.vue
+++ components/common/modal.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
-    transition(name="c-modal--fade")
-        .c-modal(v-if="val",@click='outClick')
-            .c-modal__wrapper(@click.stop,:style="{width:width}")
-                .c-modal__header
-                    slot(name="header")
-                        span.c-modal__title {{title}}
-                    span.c-modal__close(@click="val=false")
-                        i.fa.fa-close
-                .c-modal__body
-                    slot
-                .c-modal__footer
-                    slot(name="footer")
-                        .c-modal__button-wrapper
-                            button.c-modal__button.primary(@click="confirm") 确定
-                            button.c-modal__button(@click="cancel") 取消
+    .c-modal(tabindex="-1",v-if="val",@click="outClick",@keyup.esc="escPress",:class="classes")
+        .c-modal--wrapper(@click.stop,:style="styles")
+            span.c-modal__close(@click="close")
+                i.fa.fa-close
+            .c-modal__header
+                slot(name="header")
+                    h1.c-modal__title {{title}}
+            .c-modal__body
+                slot
+            .c-modal__footer
+                slot(name="footer")
+                    .c-modal__button
+                        c-button(@click="$emit('on-confirm')",type="primary") 确定
+                        c-button(@click="$emit('on-cancel')") 取消
 </template>
 <script>
+  import CButton from './button'
 
   export default {
     name: 'c-modal',
@@ -31,20 +31,37 @@
       width: {
         type: String,
         default: '40%'
+      },
+      top: {
+        type: String,
+        default: '10%'
+      },
+      // 禁用滚动条
+      lockScroll: {
+        type: Boolean,
+        default: true
+      },
+      closeOnClickMask: {
+        type: Boolean,
+        default: true
+      },
+      closeOnPressESC: {
+        type: Boolean,
+        default: true
       }
     },
     data () {
       return {
-        bodyOverflow: null,
-        val: this.value
+        val: this.value,
+        bodyOverflow: null
       }
     },
     watch: {
       val (v) {
         if (v) {
-          this.stopScroll()
-        } else {
-          this.canScroll()
+          this.$nextTick(() => {
+            this.$el.focus()
+          })
         }
         this.$emit('input', v)
       },
@@ -52,106 +69,120 @@
         this.val = val
       }
     },
-    methods: {
-      confirm () {
-        this.$emit('confirm')
-        this.val = false
+    computed: {
+      classes () {
+        const cls = []
+
+        return cls
       },
-      cancel () {
-        this.$emit('cancel')
+      styles () {
+        let width = this.width
+        if (typeof width === 'number') {
+          width = width + 'px'
+        }
+        return {
+          width,
+          margin: `${this.top} auto auto`
+        }
+      }
+    },
+    methods: {
+      close () {
         this.val = false
       },
       outClick () {
-        this.val = false
+        if (this.closeOnClickMask) {
+          this.val = false
+        }
       },
-      stopScroll () {
-        const el = document.documentElement || document.body
-        el.style.overflow = 'hidden'
-      },
-      canScroll () {
-        const el = document.documentElement || document.body
-        el.style.overflow = this.bodyOverflow
+      escPress () {
+        if (this.closeOnPressESC) {
+          this.val = false
+        }
       }
     },
     mounted () {
-      const el = document.documentElement || document.body
-      this.bodyOverflow = el.style.overflow
+      if (this.val) {
+        this.$nextTick(() => {
+          this.$el.focus()
+        })
+      }
+      this.bodyOverflow = document.body.style.overflow
+      if (this.lockScroll) {
+        document.body.style.overflow = 'hidden'
+      }
+    },
+    update () {
+      if (this.lockScroll) {
+        document.body.style.overflow = 'hidden'
+      }
     },
     beforeDestroy () {
-      this.canScroll()
+      document.body.style.overflow = this.bodyOverflow
+    },
+    components: {
+      CButton
     }
   }
 </script>
 <style lang="scss">
     @import "~assets/scss/variables";
-
-    .c-modal--fade-enter-active,
-    .c-modal--fade-leave-active {
-        transition: transform 0.4s ease-in-out;
-    }
-
-    .c-modal--fade-enter,
-    .c-modal--fade-leave-to {
-        transform: translateY(-100%);
-    }
-
-    .c-modal--fade-enter-to,
-    .c-modal--fade-leave {
-        transform: translateY(0);
-    }
+    @import "~assets/scss/mixins";
 
     .c-modal {
         position: fixed;
         left: 0;
+        right: 0;
         top: 0;
+        border: 0;
         width: 100%;
         height: 100%;
-        background-color: $modal-shadow-color;
-        .c-modal__wrapper {
+        background-color: $modal-mask-bg-color;
+        overflow: auto;
+        .c-modal--wrapper {
+            position: relative;
             background-color: $modal-bg-color;
-            border: 1px solid $border-color;
-            border-radius: $border-color;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            > * {
+            border-radius: $border-radius;
+            box-shadow: $modal-box-shadow;
+
+            .c-modal__close {
+                position: absolute;
+                right: 0.5em;
+                top: 0.5em;
                 padding: 0.5em;
-            }
-            .c-modal__header {
-                position: relative;
-                border-bottom: 1px solid $border-color;
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: space-between;
-                .c-modal__title {
-                    font-weight: bold;
+                cursor: pointer;
+                color: $modal-close-color;
+                transition: color 0.2s ease-in-out;
+                &:hover {
+                    color: $modal-close-color--hover;
                 }
-                .c-modal__close {
-                    display: block;
-                    cursor: pointer;
-                    padding: 0.5em;
-                    color: $modal-close-color;
-                    &:hover {
-                        color: $modal-close-color--hover;
-                    }
+            }
+
+            .c-modal__header {
+                border-bottom: 1px solid $border-color;
+                padding: 1em;
+                .c-modal__title {
+                    margin: 0;
+                    font-size: 1.2em;
+                    font-weight: bold;
                 }
             }
             .c-modal__body {
-
+                padding: 1em;
             }
             .c-modal__footer {
                 border-top: 1px solid $border-color;
-                .c-modal__button-wrapper {
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: flex-end;
-                    .primary {
-                        margin-right: 0.5em;
+                padding: 1em;
+                .c-modal__button {
+                    @include clearfix;
+                    .c-button {
+                        float: right;
+                        margin-left: 1em;
                     }
+
                 }
             }
         }
     }
+
 </style>
